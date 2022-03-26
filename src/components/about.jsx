14a15aa89fba44cee18c09b4af5e31a4b97ee5f8
tchebagual71 <tobydieckman@gmail.com>
2022-03-25 19:35:07 -0600

    welcome frens

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>Welcome Frens</h2>
+              <h2>About Pepefrens</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
 
             </div>
